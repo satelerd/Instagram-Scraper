fa87e007c5b77f18b99ad2121ac9cfdf2245deda
Daniel <satelerd@gmail.com>
2021-07-03 20:48:36 -0400

    mejora sustancial del codigo

--- Web Scraping.py
+++ Web Scraping.py
@@ -7,15 +7,18 @@ import xlsxwriter
 
 # AQUI DEBES LLENAR LOS DATOS
 # -------------------------------------------------------
-profile = "teletrece" # AQUI VA EL NOMBRE DEL PERFIL QUE QUIERES HACER SCRAPE (sin @)
-username = "sateler" # AQUI VA TU NOMBRE DE USUARIO DE INSTAGRAM
-password = "sat10920" # AQUI VA TU CLAVE DE INSTAGRAM
-n_post = 50 # AQUI VA LA CANTIDAD DE PUBLICACIONES A LAS QUE SE QUIERE HACER SCRAPE
-PATH = "C:\Program Files (x86)\chromedriver.exe"  # Direccion de donde tienes instalado el driver en tu computador
+profile = ""    # AQUI VA EL NOMBRE DEL PERFIL QUE QUIERES HACER SCRAPE (sin @)
+username = ""   # AQUI VA TU NOMBRE DE USUARIO DE INSTAGRAM
+password = ""   # AQUI VA TU CLAVE DE INSTAGRAM
+n_post = 5  # AQUI VA LA CANTIDAD DE PUBLICACIONES A LAS QUE SE QUIERE HACER SCRAPE (ej. si pongo 50, va a hacer scrape en las ultimas 50 publicaciones)
+
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"    # Direccion de donde tienes instalado el driver en tu computador
+wb_PATH = ""    # Direccion de donde quieres que se guarde el archivo excel. Dejar vacio para guardarlo en el mismo directorio que este archivo.
 # -------------------------------------------------------
 
 # WEB SCRAPING
-tiempo1 = time.time()
+time1 = time.time()
 
 driver = webdriver.Chrome(PATH)  # Usar el driver en Chrome
 
@@ -25,16 +28,38 @@ sleep(2)
 
 # Inicia sesíon
 user = driver.find_element_by_name("username")
-user.send_keys(username)  
+user.send_keys(username)
 pwd = driver.find_element_by_name("password")
-pwd.send_keys(password)  
+pwd.send_keys(password)
 pwd.send_keys(Keys.RETURN)  # Apreta enter
 sleep(5)
 
 # FUNCIONES
+
 # Busca el perfil
 def finder(profilex):
     driver.get(f"https://www.instagram.com/{profilex}/")
+
+
+# Correcion a los numeros cuando tienen simbolos o letras
+def num_correction(metric):
+    correction = ""
+    comma = False
+    for num in metric:
+        if num == "." or num == ",":
+            comma = True
+            continue
+        elif num == "k":
+            correction += "000"
+        elif num == "m":
+            if comma == True:
+                correction += "00000"
+            else:
+                correction += "000000"
+            break
+        else:
+            correction += num
+    return int(correction)
 
 
 # Obtiene el numero de seguidores de la cuenta
@@ -45,60 +70,36 @@ def topbar():
     for element in selenium_code_lenpost:
         elements.append(element.text)
 
-    # Cantidad de publicaciones
-    lenpost = ""
-    for num in elements[0]:  # Para poder borrar la coma
-        if num != ",":
-            lenpost += num
-        else:
-            continue
-    lenpost = int(lenpost)
-
-    # Cantidad de seguidores
-    n_followers = ""
-    for num in elements[1]:  # Para poder borrar la coma
-        if num != ",":
-            n_followers += num
-        else:
-            continue
-
-    # Cantidad de seguidos
-    n_follows = ""
-    for num in elements[2]:  # Para poder borrar la coma
-        if num != ",":
-            n_follows += num
-        else:
-            continue
-    n_follows = int(n_follows)
+    lenpost = num_correction(elements[0])   # Cantidad de publicaciones
+    n_followers = num_correction(elements[1])   # Cantidad de seguidores
+    n_follows = num_correction(elements[2])   # Cantidad de seguidos
 
     return lenpost, n_followers, n_follows
 
 
 # Consigue los links, likes y fecha de cada publicacion
-def post_to_post(lenpost):
-    # selenium_code_fechas = driver.find_elements_by_tag_name("img")  # Te entrega los links pero en formato selenium
-
+def post_to_post():
     # Variables
     links = []
     likes = []
     comments = []
     cont_post = 1
-    repetitions = int((lenpost * 2) / 36) - 1
-    print(repetitions)
 
-    for i in range(repetitions):
-        selenium_code_links = driver.find_elements_by_tag_name("a")  # Te entrega los links pero en formato selenium
+    for i in range(n_post):
+        selenium_code_links = driver.find_elements_by_tag_name(
+            "a")  # Te entrega los links pero en formato selenium
         selenium_code_links = driver.find_elements_by_tag_name("a")
 
-        
         # Links y Likes
         for publicacion in selenium_code_links:
-            link = publicacion.get_attribute("href")  # Lo pasa a formato html y lo appendea a links_total
+            # Lo pasa a formato html y lo appendea a links_total
+            link = publicacion.get_attribute("href")
 
             if link in links:
                 continue
 
             if "/p/" in link:  # Filtra los links que no son publicaciones
+                cont_post += 1
 
                 # Links
                 links.append(link)  # Apendea los links necesarios
@@ -107,24 +108,17 @@ def post_to_post(lenpost):
                 hover = ActionChains(driver).move_to_element(publicacion)
                 hover.perform()
 
-                print(f"publicacion {cont_post}")
-                cont_post += 1
-                
                 code = driver.find_elements_by_class_name("-V_eO")
 
-                print(code)
-                
-                likes.append(code[0].text)
-                comments.append(code[1].text)
+                likes.append(num_correction(code[0].text))
+                comments.append(num_correction(code[1].text))
 
-                if cont_post == n_post:
-                    print("se llego a limite")
+                if cont_post == n_post + 1:
                     break
 
-        if cont_post == n_post:
-            print("se llego a limite")
+        if cont_post == n_post + 1:
             break
-     
+
         # Scroll
         html = driver.find_element_by_tag_name('html')
         html.send_keys(Keys.END)
@@ -140,12 +134,14 @@ def tab(links):
 
     for link in links:
         driver.execute_script("window.open('');")  # Abre una nueva pestaña
-        driver.switch_to.window(driver.window_handles[1])  # Cambia a la nueva pestaña
+        driver.switch_to.window(driver.window_handles[1])   # Cambia a la nueva pestaña
         driver.get(link)  # Abre el link
 
         # Fechas
-        selenium_code = driver.find_element_by_tag_name("time")  # Te entrega los datos pero en formato selenium
-        dates.append(selenium_code.get_attribute("title"))  # Lo pasa a formato html y lo appendea a fechas
+        # Te entrega los datos pero en formato selenium
+        selenium_code = driver.find_element_by_tag_name("time")
+        # Lo pasa a formato html y lo appendea a fechas
+        dates.append(selenium_code.get_attribute("title"))
 
         # Aria label
         try:
@@ -163,19 +159,19 @@ def tab(links):
 
 # DATA
 finder(profile)
-lenpost_tz, seguidores_tz, seguidos_tz = topbar()
-links_tz, likes_tz, comments_tz = post_to_post(lenpost_tz)
-dates, aria_label_tz = tab(links_tz)
+lenpost_prf, seguidores_prf, seguidos_prf = topbar()
+links_prf, likes_prf, comments_prf = post_to_post()
+dates_prf, aria_label_prf = tab(links_prf)
 
 # Prints
-print(f"{lenpost_tz} publicaciones")
-print(f"{seguidores_tz} seguidores")
-print(f"{seguidos_tz} seguidos")
-print(f"Los links de los post son: {links_tz}")
-print(f"los likes de los post son: {likes_tz}")
-print(f"El numero de comments por cada post son: {comments_tz}")
-print(f"la fecha de los post son: {dates}")
-print(f"El area label de por cada post son: {aria_label_tz}")
+print(f"{lenpost_prf} publicaciones")
+print(f"{seguidores_prf} seguidores")
+print(f"{seguidos_prf} seguidos")
+print(f"Los links de los post son: {links_prf}")
+print(f"los likes de los post son: {likes_prf}")
+print(f"El numero de comments por cada post son: {comments_prf}")
+print(f"la fecha de los post son: {dates_prf}")
+print(f"El area label de por cada post son: {aria_label_prf}")
 
 driver.quit()
 # FIN DEL SCRAPE
@@ -183,49 +179,44 @@ driver.quit()
 
 # EXCEL
 # Crea el file
-wb = xlsxwriter.Workbook("Web Scraping Instagram.xlsx")
+wb = xlsxwriter.Workbook(f"{wb_PATH}Ws_IG - {profile}.xlsx")
 sheet = wb.add_worksheet()
 
 # Formatos
-color_tz = wb.add_format({"bg_color": "#94e5ff", "border": 2})
+bg_color = wb.add_format({"bg_color": "#0099FF", "border": 2})
 border1 = wb.add_format({"border": 1})
 border2 = wb.add_format({"border": 2})
 
 # Creamos las pestañas de tipo de datos
-sheet.write(0, 0, "Cuenta", border2)
-sheet.write(0, 1, "Tipo de post", border2)
-sheet.write(0, 2, "Likes", border2)
-sheet.write(0, 3, "comments", border2)
-sheet.write(0, 4, "Fecha", border2)
-sheet.write(0, 5, "Link", border2)
+sheet.write(0, 0, "Tipo de post", border2)
+sheet.write(0, 1, "Likes", border2)
+sheet.write(0, 2, "comments", border2)
+sheet.write(0, 3, "Fecha", border2)
+sheet.write(0, 4, "Link", border2)
 
 # Numero de post, seguidores y seguidos
 sheet.write(0, 8, "Publicaciones", border2)
 sheet.write(0, 9, "Seguidores", border2)
 sheet.write(0, 10, "seguidos", border2)
 
-sheet.write(1, 7, f"Tzeirei Ami", color_tz)
+sheet.write(1, 7, f"{profile}", bg_color)
 
 # Rows
-
 row = 1
 
-# Rows Tzeirei
-sheet.write(1, 8, lenpost_tz, border1)
-sheet.write(1, 9, seguidores_tz, border1)
-sheet.write(1, 10, seguidos_tz, border1)
+sheet.write(1, 8, lenpost_prf, border1)
+sheet.write(1, 9, seguidores_prf, border1)
+sheet.write(1, 10, seguidos_prf, border1)
 
-for i in range(len(links_tz)):
-    sheet.write(i + row, 0, "tzeireiami", color_tz)  # Cuenta
-    sheet.write(i + row, 1, aria_label_tz[i], border1)  # Tipo de post
-    sheet.write(i + row, 2, likes_tz[i], border1)  # Likes
-    sheet.write(i + row, 3, int(comments_tz[i]), border1)  # comments
-    sheet.write(i + row, 4, dates[i], border1)  # Fecha
-    sheet.write(i + row, 5, links_tz[i], border1)  # Links
-row += len(links_tz)
+for i in range(len(links_prf)):
+    sheet.write(i + row, 0, aria_label_prf[i], border1)  # Tipo de post
+    sheet.write(i + row, 1, likes_prf[i], border1)  # Likes
+    sheet.write(i + row, 2, comments_prf[i], border1)  # comments
+    sheet.write(i + row, 3, dates_prf[i], border1)  # Fecha
+    sheet.write(i + row, 4, links_prf[i], border1)  # Links
+row += len(links_prf)
 
 wb.close()
 
-tiempo2 = time.time()
-
-print(f"Fueron: {int(tiempo2 - tiempo1)} segundos")
+time2 = time.time()
+print(f"Fueron: {int(time2 - time1)} segundos")
